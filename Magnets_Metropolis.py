780f5e98d6adf7a95011b19806352fb370f90178
Juan Felipe <juangelr@unal.edu.co>
2025-12-02 13:05:07 -0500

    Implementation of for cycle to evaluate multiple temperatures and save the data of termo quantitis in the equilibrium.

--- Magnets_Metropolis.py
+++ Magnets_Metropolis.py
@@ -4,37 +4,47 @@ import Functions as Fc
 import numpy as np
 
 
-##Defining the state vector of spins that create the material (they're fixed, we don't care about positions or momenta)
-# and defining the evolution Matrix (it's a matrix since it has to store the state chain vector at each time in order to visualize it)
+for case in range(len(Fc.Values["T"])):
 
-Chain_Spin_StateVect = np.full(Fc.Values["Num_particles"],1)
-Evol_Matr = np.zeros((Fc.Values["Num_particles"], Fc.Values["total_Time"]))
+    ##Defining the state vector of spins that create the material (they're fixed, we don't care about positions or momenta)
+    # and defining the evolution Matrix (it's a matrix since it has to store the state chain vector at each time in order to visualize it)
 
-#print(Chain_Spin_StateVect)
-#print(Evol_Matr)
+    Chain_Spin_StateVect = np.full(Fc.Values["Num_particles"],1)
+    Evol_Matr = np.zeros((Fc.Values["Num_particles"], Fc.Values["total_Time"]))
 
-Evol_Matr[:,0] = np.transpose(Chain_Spin_StateVect) # Matrix where all states will be saved
+    #print(Chain_Spin_StateVect)
+    #print(Evol_Matr)
 
-E_equil_verf = []                                   # Vector where I'm going to save energy differences to check equilibration
-E_equil_verf.clear()
+    Evol_Matr[:,0] = np.transpose(Chain_Spin_StateVect) # Matrix where all states will be saved
 
-E_equil_verf.append(Fc.State_Energy(Chain_Spin_StateVect))  # Save energy of the initial state
+    E_equil_verf = []                                   # Vector where I'm going to save energy differences to check equilibration
+    E_equil_verf.clear()
 
-
-##Evolution of the state vector (Metropolis)
-
-time = np.arange(0, Fc.Values["total_Time"], 1)
-
-Final_evol = Fc.Metropoolis(time, Chain_Spin_StateVect, Evol_Matr, E_equil_verf, Fc.Values["Start_quantit_calcu"])
+    E_equil_verf.append(Fc.State_Energy(Chain_Spin_StateVect))  # Save energy of the initial state
 
 
-##Save data to graph
+    ##Evolution of the state vector (Metropolis)
 
-print(Final_evol[2])
-print(Final_evol[3])
-print(Final_evol[4])
+    time = np.arange(0, Fc.Values["total_Time"], 1)
 
-Data_checkEnergy_Equi = np.column_stack((time, Final_evol[1]))
+    Final_evol = Fc.Metropoolis(time, Chain_Spin_StateVect, Evol_Matr, E_equil_verf, Fc.Values["Start_quantit_calcu"], Fc.Values["T"][case])
 
-np.savetxt('StateEvol.txt', Final_evol[0])              #Save the evolution of the initial state data in a txt to graph
-np.savetxt('AvrEnergy.txt', Data_checkEnergy_Equi)      #Save the Average Energy values in each state to check equilibrium to graph
+
+    ##Save data to graph
+
+    #print(Final_evol[2])
+    #print(Final_evol[3])
+    #print(Final_evol[4])
+
+    Final_evol_array = np.array(Final_evol[2:])
+
+    if case == 0:
+        np.savetxt('Termo_values.txt', Final_evol_array.reshape(1, -1), header='Magnetization Internal_Energy Specific_heat')
+    else:
+        with open('Termo_values.txt', 'a') as f:
+            np.savetxt(f, Final_evol_array.reshape(1, -1))
+
+    Data_checkEnergy_Equi = np.column_stack((time, Final_evol[1]))
+
+    np.savetxt('StateEvol.txt', Final_evol[0])              #Save the evolution of the initial state data in a txt to graph
+    np.savetxt('AvrEnergy.txt', Data_checkEnergy_Equi)      #Save the Average Energy values in each state to check equilibrium to graph
