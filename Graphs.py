472bf8bff5fe2735b53dea608d2bb59f1460478d
alel <jlancerosl@sala21.salafis.net>
2025-12-02 10:25:15 -0500

    addition of the grafication of the analitic solution applied to our values

--- Graphs.py
+++ Graphs.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import Functions as Fc
 
 ##Read data
 EV_matr = np.loadtxt('StateEvol.txt')
@@ -29,4 +30,43 @@ plt.xlabel('Time')
 plt.ylabel('Avr. Energy')
 plt.grid()
 #fig2.savefig('Avr_Energy_equilCheck.pdf')
+plt.show()
+
+
+##graph analytic solution
+
+n=Fc.Values["Num_particles"]
+j=Fc.Values["J"]
+k=Fc.Values["K"]
+t=np.arange(0.1,5,0.1)
+b=Fc.Values["B"]
+
+def u_ana(n,j,k,t):
+    return -n*j*np.tanh(j/(k*t))
+
+#print(u_ana(n,j,k,t))
+
+def cal_ana(n,j,k,t):
+    return (j/(k*t))**2/(np.cosh(j/(k*t)))
+
+#print(cal_ana(n,j,k,t))
+
+def mag_ana(n,j,k,t,b):
+    return n*np.exp(j/(k*t))*np.sinh(b/(k*t))/np.sqrt(np.exp(2*j/(k*t))*(np.sinh(b/(k*t)))**2 + np.exp(-2*j/(k*t)))
+
+#print(mag_ana(n,j,k,t,b))
+
+plt.figure()
+plt.title('cálculo analítico de la energía')
+plt.plot(t,u_ana(n,j,k,t))
+plt.show()
+
+plt.figure()
+plt.title('cálculo analítico del calor específico')
+plt.plot(t,cal_ana(n,j,k,t))
+plt.show()
+
+plt.figure()
+plt.title('cálculo analítico de la magentización')
+plt.plot(t,mag_ana(n,j,k,t,b))
 plt.show()
